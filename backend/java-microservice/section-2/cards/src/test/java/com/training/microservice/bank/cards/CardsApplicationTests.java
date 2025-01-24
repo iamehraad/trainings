7c66e73e2dc984d39310b78cbe6f82eaccc4dbb9
Mehrad Alinezhad <iamehraad@gmail.com>
2025-01-24 19:29:02 +0100

    Added cards service

---
+++ backend/java-microservice/section-2/cards/src/test/java/com/training/microservice/bank/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.microservice.bank.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
