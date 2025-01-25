b311d4c2b165b4b305641570bc90d376734c5074
Mehrad Alinezhad <iamehraad@gmail.com>
2025-01-25 10:46:26 +0100

    Added audit and constants

--- backend/java-microservice/section-2/cards/src/main/java/com/training/microservice/bank/cards/audit/AuditAwareImpl.java
+++ backend/java-microservice/section-2/cards/src/main/java/com/training/microservice/bank/cards/audit/AuditAwareImpl.java
@@ -1,4 +1,14 @@
 package com.training.microservice.bank.cards.audit;
 
-public class AuditAwareImpl {
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
 }
