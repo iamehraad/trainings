6105d09c230da271b915c74db8929f10bec49381
Mehrad Alinezhad <iamehraad@gmail.com>
2025-03-15 10:51:18 +0100

    Add domain classes

---
+++ backend/bookstore/src/main/kotlin/com/training/bookstore/domain/Author.kt
@@ -0,0 +1,9 @@
+package com.training.bookstore.domain
+
+class Author (
+    var id: Long?,
+    var name: String,
+    var age: Int,
+    var description: String,
+    var image: String,
+)
---
+++ backend/bookstore/src/main/kotlin/com/training/bookstore/domain/Book.kt
@@ -0,0 +1,9 @@
+package com.training.bookstore.domain
+
+class Book (
+    var isbn: String,
+    var title: String,
+    var description: String,
+    var image: String,
+    var author: Author,
+)
