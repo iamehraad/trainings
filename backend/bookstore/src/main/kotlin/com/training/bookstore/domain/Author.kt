da63db2d28c4d492f93f0e7fc57f42c398eab869
Mehrad Alinezhad <iamehraad@gmail.com>
2025-03-15 13:38:23 +0100

    fixed entities

--- backend/bookstore/src/main/kotlin/com/training/bookstore/domain/Author.kt
+++ backend/bookstore/src/main/kotlin/com/training/bookstore/domain/Author.kt
@@ -1,9 +1,26 @@
 package com.training.bookstore.domain
 
-data class Author (
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "authors")
+data class Author(
+    @Id
+    @Column(name = "id")
     val id: Long?,
+
+    @Column(name = "name")
     val name: String,
+
+    @Column(name = "age")
     val age: Int,
+
+    @Column(name = "description")
     val description: String,
+
+    @Column(name = "image")
     val image: String,
 )
--- backend/bookstore/src/main/kotlin/com/training/bookstore/domain/Book.kt
+++ backend/bookstore/src/main/kotlin/com/training/bookstore/domain/Book.kt
@@ -1,9 +1,24 @@
 package com.training.bookstore.domain
 
-data class Book (
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "books")
+data class Book(
+    @Id
+    @Column(name = "id")
     val isbn: String,
+
+    @Column(name = "title")
     val title: String,
+
+    @Column(name = "description")
     val description: String,
+
+    @Column(name = "image")
     val image: String,
+
+    @ManyToOne(cascade = [CascadeType.DETACH])
+    @JoinColumn(name = "author_id")
     val author: Author,
 )
