9f5756825ab059546ebb6f2b577b0e5b100e01d7
Mehrad Alinezhad <iamehraad@gmail.com>
2025-03-22 10:30:53 +0100

    removed books

--- backend/bookstore/src/main/kotlin/com/training/bookstore/repositories/BookRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.training.bookstore.repositories
-
-import com.training.bookstore.domain.entities.BookEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface BookRepository : JpaRepository<BookEntity, String>
